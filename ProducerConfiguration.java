1aac98832f154db1609c0efacb4e1034ab7abce4
Swatij1999 <114226950+Swatij1999@users.noreply.github.com>
2023-01-14 00:06:45 +0530

    Add files via upload

--- ProducerConfiguration.java
+++ ProducerConfiguration.java
@@ -6,18 +6,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-
-
-public class OptimisticConfiguration {
-	
+public class ProducerConfiguration {
 	@Configuration
 	@ComponentScan(basePackages="com.xworkz.sprigReference",
-			excluders= {@ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE,value=Collection.class)})
+			excludeFilters= {@ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE,value=Collection.class)})
 
 	
-	public OptimisticConfiguration() {
-		System.out.println("creating OptimisticConfiguration with no argument constructor");
-	}
-	
+	public ProducerConfiguration() {
+	System.out.println("creating producer with no argument constructor");
 
+}
 }
