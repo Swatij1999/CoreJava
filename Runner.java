f1829c11b1043a25234ec918fe907da13d2783ec
Swatij1999 <114226950+Swatij1999@users.noreply.github.com>
2023-01-14 00:05:54 +0530

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,27 +1,37 @@
-package com.xworkz.autowired;
+package com.xworkz.friday.boot;
 
 import java.util.Arrays;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.xworkz.friday.Configuration.ProducerConfiguration;
+import com.xworkz.friday.bean.Assistant;
+import com.xworkz.friday.bean.CameraMan;
+import com.xworkz.friday.bean.Movie;
+import com.xworkz.friday.bean.OptimisticConfiguration;
+import com.xworkz.friday.bean.Polymath;
+
 public class Runner {
+	public static void main(String[]args) {
+	ApplicationContext spring=new AnnotationConfigApplicationContext(OptimisticConfiguration.class);
+	System.out.println(Arrays.toString(spring.getBeanDefinitionNames()));
+	
+	Polymath ref=spring.getBean(Polymath.class);
+	System.out.println(ref);
+	
+	ApplicationContext spring1=new AnnotationConfigApplicationContext(ProducerConfiguration.class);
+	System.out.println(Arrays.toString(spring1.getBeanDefinitionNames()));
+	
+	Assistant ref1=spring.getBean(Assistant.class);
+	System.out.println(ref1);
+	
+	CameraMan ref2=spring.getBean(CameraMan.class);
+	System.out.println(ref2);
+	
+	Movie ref3=spring.getBean(Movie.class);
+	System.out.println(ref3);
+	
 
-	public static void main(String[] args) {
-		
-		ApplicationContext container = new AnnotationConfigApplicationContext(FirstTimeConfiguration.class);
-		Item ref = container.getBean("material",Item.class);
-		System.out.println(ref.getName());
-		
-		
-		String[] beanNames = container.getBeanDefinitionNames();
-		System.out.println(Arrays.toString(beanNames));
-		
-		Experience ref1 =container. getBean(Experience.class);
-		
-		System.out.println(ref1.getNoOfYears());//10
-		System.out.println(ref1.getRole());//tester
-		
 	}
-
 }
