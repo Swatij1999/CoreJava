b556a098970572a42abee53f89013049d9c6bd87
Swatij1999 <114226950+Swatij1999@users.noreply.github.com>
2023-01-19 23:04:41 +0530

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,21 +1,35 @@
-package com.xworkz.springFramework.boot;
-
-import java.util.Arrays;
+package com.xworkz.boot;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.xworkz.springFramework.configuration.SpringConfiguration;
-
+import com.xworkz.configuration.SpringConfiguration;
+import com.xworkz.dto.FirstAidDTO;
+import com.xworkz.dto.MissileDTO;
+import com.xworkz.dto.ResortDTO;
+import com.xworkz.service.FirstAidService;
+import com.xworkz.service.MissileService;
+import com.xworkz.service.ResortService;
 
 public class Runner {
-	public static void main(String[]args) {
-		
 	
-		ApplicationContext spring=new AnnotationConfigApplicationContext(SpringConfiguration.class);
-		System.out.println(Arrays.toString(spring.getBeanDefinitionNames()));
-		System.out.println(spring.getBeanDefinitionCount());
-	}
+	public static void main(String[]args) {
+		ApplicationContext container = new AnnotationConfigApplicationContext(SpringConfiguration.class);
 		
+		System.out.println("Missile Runner");
+		MissileService service=container.getBean(MissileService.class);
+		boolean saved = service.validateAndSave(new MissileDTO());
+		System.out.println("saved"+saved);
+		
+		System.out.println("FirstAid Runner");
+		FirstAidService service1=container.getBean(FirstAidService.class);
+		boolean saved1 = service1.validateAndSave(new FirstAidDTO());
+		System.out.println("saved"+saved1);
+		
+		System.out.println("Resort Runner");
+		ResortService service11=container.getBean(ResortService.class);
+		boolean saved11 = service11.validateAndSave(new ResortDTO());
+		System.out.println("saved"+saved11);
+	}
 
 }
