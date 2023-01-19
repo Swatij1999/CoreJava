3f02c3cf10e50745fa1418ab47b8fc4a46225885
Swatij1999 <114226950+Swatij1999@users.noreply.github.com>
2023-01-19 23:05:53 +0530

    Add files via upload

--- SpringConfiguration.java
+++ SpringConfiguration.java
@@ -1,10 +1,23 @@
-package com.xworkz.springFramework.configuration;
+package com.xworkz.configuration;
 
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.xworkz.springFramework")
+@ComponentScan(basePackages="com.xworkz")
 public class SpringConfiguration {
+	
+	@Bean
+	public Validator validator() {
+		System.out.println("registerting validator");
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		return validator;
+	}
 
 }
